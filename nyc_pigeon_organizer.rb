1be99b7c92636a9feaa1865f62e488e706341fd5
Robert Obi <robobi97@googlemail.com>
2021-01-04 04:01:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,3 @@
 def nyc_pigeon_organizer(data)
-pigeon_data = {
-  :color => {
-    :brown => ["Luca"],
-    :black => ["Lola"],
-  },
-  :gender => {
-    :male => ["Luca"],
-    :female => ["Lola"]
-  },
-  :lives => {
-    "Central Park" => ["Lola"],
-    "Library" => ["Luca"]
-  }
-}
+  # write your code here!
 end
